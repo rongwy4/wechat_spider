429123c6352158aa9c59e3f7bc8c1b9d9d25b311
周涛 <zhoutaoo@foxmail.com>
2018-11-04 14:44:58 +0800

    升级anyproxy

--- index.js
+++ index.js
@@ -3,20 +3,23 @@ var proxy = require("anyproxy");
 module.exports = function () {
     //create cert when you want to use https features
     //please manually trust this rootCA when it is the first time you run it
-    !proxy.isRootCAFileExists() && proxy.generateRootCA(function(){});
+    !proxy.utils.certMgr.ifRootCAFileExists() && proxy.utils.certMgr.generateRootCA(function () {
 
+    });
+    console.log(proxy.utils.certMgr.ifRootCAFileExists());
     var options = {
-        type          : "http",
-        port          : 8001,
-        hostname      : "localhost",
-        rule          : require("./lib/rule_wechat_spider.js"),
-        dbFile        : null,  // optional, save request data to a specified file, will use in-memory db if not specified
-        webPort       : 8002,  // optional, port for web interface
-        socketPort    : 8003,  // optional, internal port for web socket, replace this when it is conflict with your own service
-        disableWebInterface : false, //optional, set it when you don't want to use the web interface
-        setAsGlobalProxy : false, //set anyproxy as your system proxy
-        silent        : false, //optional, do not print anything into terminal. do not set it when you are still debugging.
-        interceptHttps: true
+        type: "http",
+        port: 8001,
+        webInterface: {
+            enable: true,
+            webPort: 8002
+        },
+        throttle: 10000,
+        rule: require("./lib/rule_wechat_spider.js"),
+        dbFile: null,  // optional, save request data to a specified file, will use in-memory db if not specified
+        silent: false, //optional, do not print anything into terminal. do not set it when you are still debugging.
+        forceProxyHttps: true
     };
-    new proxy.proxyServer(options);
+    var server = new proxy.ProxyServer(options);
+    server.start();
 };
